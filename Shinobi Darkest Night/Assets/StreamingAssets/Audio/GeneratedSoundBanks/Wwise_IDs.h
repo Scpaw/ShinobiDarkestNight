e96075d43d8493e300ee9eb9f7c6a3869162daeb
zalex <zaleks.production@gmail.com>
2023-08-31 21:25:46 +0200

    Test Events and general wwise adjustments
    Play Test beep 2d and 3d, loop and one shot. Wwise adjustments: Switches, game parameters, states, master-mixer and actor-mixer hierarchy adjustments

--- Shinobi Darkest Night/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Shinobi Darkest Night/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -16,6 +16,78 @@ namespace AK
         static const AkUniqueID PLAYER_DASH = 2394582229U;
     } // namespace EVENTS
 
+    namespace STATES
+    {
+        namespace AREA_STATE
+        {
+            static const AkUniqueID GROUP = 3979687176U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUTDOORS = 2730119150U;
+            } // namespace STATE
+        } // namespace AREA_STATE
+
+        namespace GAMESTATUS
+        {
+            static const AkUniqueID GROUP = 1045871717U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INGAME = 984691642U;
+                static const AkUniqueID INMENU = 3374585465U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace GAMESTATUS
+
+        namespace PLAYER_STATE
+        {
+            static const AkUniqueID GROUP = 4071417932U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYER_STATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace PLAYERHEALTH
+        {
+            static const AkUniqueID GROUP = 151362964U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FULLHEALTH = 2429688720U;
+                static const AkUniqueID LOWHEALTH = 1017222595U;
+                static const AkUniqueID NEARDEATH = 898449699U;
+            } // namespace SWITCH
+        } // namespace PLAYERHEALTH
+
+        namespace PLAYERSPEEDSWITCH
+        {
+            static const AkUniqueID GROUP = 2051106367U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID RUN = 712161704U;
+                static const AkUniqueID WALK = 2108779966U;
+            } // namespace SWITCH
+        } // namespace PLAYERSPEEDSWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_DISTANCE = 262290038U;
+        static const AkUniqueID RTPC_PLAYERSPEED = 2653406601U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -24,8 +96,24 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID _2DAMBIENCE = 309309195U;
+        static const AkUniqueID _2DAMBIENTBEDS = 4152869693U;
+        static const AkUniqueID _3DAMBIENCE = 1301074112U;
+        static const AkUniqueID AMBIENTBEDS = 1182634443U;
+        static const AkUniqueID AMBIENTMASTER = 1459460693U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID NPCMASTER = 2033911932U;
+        static const AkUniqueID PLAYERCLOTH = 765206498U;
+        static const AkUniqueID PLAYERLOCOMOTION = 2343802269U;
+        static const AkUniqueID PLAYERMASTER = 3538689948U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CAVE = 4122393694U;
+        static const AkUniqueID OUTDOOR = 144697359U;
+        static const AkUniqueID REVERBS = 3545700988U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
